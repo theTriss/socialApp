3cc9dc34b651349aa6d62dabfcf59f558a599494
denis <busha9077511@gmail.com>
2019-09-17 14:46:51 +0300

    Updates

---
+++ precache-manifest.4218781416cb1e0f79988d918deb1ab0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d5f9c971af61128063ef47eb96be862",
+    "url": "/socialApp/index.html"
+  },
+  {
+    "revision": "9fd42616579c6e8791f0",
+    "url": "/socialApp/static/css/main.3b8043f4.chunk.css"
+  },
+  {
+    "revision": "491df8136ef6fd325dc7",
+    "url": "/socialApp/static/js/2.de3ab263.chunk.js"
+  },
+  {
+    "revision": "9fd42616579c6e8791f0",
+    "url": "/socialApp/static/js/main.8973d3f6.chunk.js"
+  },
+  {
+    "revision": "f97feaf8deb1159ceb70",
+    "url": "/socialApp/static/js/runtime~main.998b9461.js"
+  }
+]);
